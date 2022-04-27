419238c28bf64aae860ab7b1f271340fd0bf20ac
mjacintosalas <mjs080@gmail.com>
2022-04-27 16:30:20 +0800

    Dependency Injection Implementation

---
+++ ShangSolutionInc.WebAPIExam/DependencyInjection/Main.cs
@@ -0,0 +1,42 @@
+﻿namespace ShangSolutionInc.WebAPIExam.DependencyInjection
+{
+    public class Main
+    {
+        public void main() 
+        {
+            ClientAUser clientAUser = new ClientAUser();
+            ClientBUser clientBUser = new ClientBUser();
+
+            checkUser(clientAUser); //Accepts ClientAUser object
+
+            checkUser(clientBUser); //Accepts ClientBUser object
+        }
+
+        public void checkUser(IUser user)
+        {
+            //code here
+        }
+    }
+
+    public interface IUser
+    { 
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+    }
+
+    public class ClientAUser : IUser
+    {
+        public string ID { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Role { get; set; }
+    }
+
+    public class ClientBUser : IUser
+    {
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Level { get; set; }
+    }
+}
